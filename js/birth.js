04d1b4275ff9c9cc19dffc4d22432d8da9cdbc2d
KWQ <3027595718@qq.com>
2024-05-13 08:58:38 +0800

    Update birth.js

--- js/birth.js
+++ js/birth.js
@@ -41,7 +41,7 @@ var S = {
       if (i !== -1) {
         S.UI.simulate(decodeURI(action).substring(i + 3));
       } else {
-        S.UI.simulate('||祝|王嘉幸|生日快乐|祝你|生日快乐|祝你幸福|祝你健康|祝你|生日快乐！');
+        S.UI.simulate('||祝|王嘉幸|生日快乐|祝你|事事如意|心想事成|鹏程万里|祝你|生日快乐！');
       }
 
       /* S.Drawing.loop(function () {
